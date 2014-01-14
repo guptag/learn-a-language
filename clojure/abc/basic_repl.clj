cbd58a2d717686cf7be7d3cf697ac04ddae223b3
guptag <guptagaruda@gmail.com>
2014-01-14 18:39:26 -0500

    functions, hashmaps, let

--- clojure/abc/basic_repl.clj
+++ clojure/abc/basic_repl.clj
@@ -1,17 +1,115 @@
+;;; Credits: http://www.amazon.com/Clojure-Action-Amit-Rathore/dp/193518259
+
+
+#_(-----------------
+   Basic constructs
+   -----------------)
+
 ;; load-file "basic_repl.clj"
 
 
 ;; hello world
 (println "hello world")
 
-;; basic hashmap lookup
-(def users {"user1" {:password "password1" :number-of-items 10}
-			"user2" {:password "password2" :number-of-items 20}
-			"user3" {:password "password3" :number-of-items 30}
-			"user4" {:password "password4" :number-of-items 40}})
+;; doc
+(doc +)
 
 
-;; (check-login "user1" "password1")
-(defn check-login [username password]
-	(let [actual-password ((users username) :password)]
-		(= actual-password password)))
+;; regex search docs
+;;(find-doc "lazy")
+
+
+;; spacing
+(+ 1 2 3)
+(+ 1, 2, 3, 4)
+
+(def a-map {:a 1 :b 2 :c 3})
+(println a-map)
+
+(def a-map-withspaces {:a 1, :b 2, :c 3})
+(println a-map-withspaces)
+
+
+
+
+#_(----------------------
+   Function Defnitions
+   ---------------------)
+
+;; regular function definition
+;; notice 'defn'
+(defn add-fn [x y]
+    (+ x y))
+
+(add-fn 2 3) ; execute function
+
+
+;; anonymous function assigned to a variable
+;; notice 'def'
+(def add-anon-fn (fn [x y]
+                   (+ x y)))
+
+(add-anon-fn 10 50) ; execute function
+
+
+
+#_(--------
+   HashMap
+   --------)
+
+;; Use 'let' to temporarily name things
+
+(def users {:User1 {:salary 60000 :title "Sr. Developer"}
+            :User2 {:salary 55000 :title "Coordinator"}
+            :User3 {:salary 50000 :title "Tech Writer"}})
+
+(println users)
+(println (keys users)) ;all keys of hashmap
+(println (vals users)) ;all values of hashmap
+(println (map :salary (vals users))) ;salary infomation of all users
+(println (apply + (map :salary (vals users)))) ;total salary of all users
+
+
+
+
+#_(--------
+   let
+   --------)
+
+;; Use 'let' to temporarily name things
+(defn average-salary [users]
+  (let [user-data (vals users)
+        all-salaries (map :salary user-data)
+        total (apply + all-salaries)]
+    (/ total (count user-data))))
+
+(average-salary {:User1 {:salary 10000}
+                 :User2 {:salary 15000}})
+
+
+
+;; another example of let
+;; declare a param that is dependent on others
+(defn another-let-example []
+  (let [first "hello"
+        last "world"
+        full (str first last)
+        ]
+    (println full)))
+
+(another-let-example)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
