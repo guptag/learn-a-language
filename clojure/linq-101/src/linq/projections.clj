0db5df382c8200147bf97c13595a3cc0fa822890
guptag <guptagaruda@gmail.com>
2014-01-28 08:07:29 -0500

    linq13, linq 14

--- clojure/linq-101/src/linq/projections.clj
+++ clojure/linq-101/src/linq/projections.clj
@@ -7,7 +7,9 @@
 
 
 #_(-------------------------------
+
   ;; // Select to produce a sequence of ints one higher than those in an existing array of ints
+
   ;; public void Linq6()
   ;; {
   ;;  int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
@@ -40,6 +42,7 @@
 #_(-------------------------------------
 
 ;; //select to return a sequence of just the names of a list of products
+
 ;; public void Linq7()
 ;; {
 ;;    List<Product> products = GetProductList();
@@ -56,7 +59,6 @@
 ;;}
 -------------------------------------)
 
-;; return names of products
 ;; explicit function for map construct
 (defn linq7a []
   (let [products products-list
@@ -66,7 +68,6 @@
 
 
 
-;; return names of products
 ;; anonymous function for map construct
 (defn linq7b []
   (let [products products-list
@@ -83,7 +84,9 @@
 
 
 #_(-------------------------------------
+
 ;; // Return text version of numbers in a sequence
+
 ;; public void Linq8()
 ;; {
 ;;    int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
@@ -115,6 +118,9 @@
 
 
 #_(----------------------------------
+
+;; // Return sequence of the uppercase and lowercase versions of each word in the original array.
+
 ;; public void Linq9()
 ;; {
 ;;    string[] words = { "aPPLE", "BlUeBeRrY", "cHeRry" };
@@ -129,8 +135,6 @@
 ;;    }
 ;; }
 ------------------------------------)
-
-;; Return sequence of the uppercase and lowercase versions of each word in the original array.
 
 ;; with map
 (defn linq9a []
@@ -156,7 +160,9 @@
 
 
 #_(---------------------------------
+
 ;;  // select to produce a sequence containing text representations of digits and whether their length is even or odd.
+
 ;;  public void Linq10()
 ;;  {
 ;;      int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
@@ -173,7 +179,6 @@
 ;;  }
 -----------------------------------)
 
-;; select to produce a sequence containing text representations of digits and whether their length is even or odd.
 (defn linq10 []
   (let [numbers [5 4 1 3 9 8 6 7 2 0]
        strings ["Zero" "One" "Two" "Three" "Four" "Five" "Six" "Seven" "Eight" "Nine"]
@@ -192,6 +197,7 @@
 
 ;; //select to produce a sequence containing some properties of Products,
 ;; //including UnitPrice which is renamed to Price in the resulting type
+
 ;; public void Linq11()
 ;;{
 ;;    List<Product> products = GetProductList();
@@ -209,9 +215,6 @@
 
 --------------------------------------)
 
-
-;; select to produce a sequence containing some properties of Products,
-;; including UnitPrice which is renamed to Price in the resulting type
 (defn linq11 []
   (let [products products-list
         product-infos
@@ -231,10 +234,10 @@
 
 
 
-
-
 #_(------------------------------------
+
 ;; // Indexed Select clause to determine if the value of ints in an array
+
 ;; // match their position in the array.
 ;; public void Linq12()
 ;; {
@@ -259,6 +262,89 @@
     (doseq [n nums-in-place]
       (println (:num n) ": " (:in-place n)))))
 
-;;(linq12)
+;; (linq12)
 
-(def examples [linq6 linq7a linq7b linq8 linq9a linq9b linq10 linq11])
+
+
+
+
+
+#_(-------------------------------------
+
+;;  //make a simple query that returns the text form of each digit less than 5.
+
+;;  public void Linq13()
+;;  {
+;;    int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+;;    string[] digits = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+;;
+;;    var lowNums =
+;;        from n in numbers
+;;        where n < 5
+;;        select digits[n];
+;;
+;;    Console.WriteLine("Numbers < 5:");
+;;    foreach (var num in lowNums)
+;;    {
+;;        Console.WriteLine(num);
+;;    }
+;;  }
+---------------------------------------------)
+
+(defn linq13 []
+  (let [numbers [5 4 1 3 9 8 6 7 2 0]
+        digits  ["zero" "one" "two" "three" "four" "five" "six" "seven" "eight" "nine"]
+        low-nums (for [n numbers
+                       :when (< n 5)]
+                  (digits n))]
+    (println "Numbers < 5")
+    (doseq [num low-nums]
+      (println num))))
+
+;;(linq13)
+
+
+
+
+
+
+
+#_(---------------------------------------------
+
+;; // query that returns all pairs of numbers from both arrays such
+;; // that the number from numbersA is less than the number from numbersB.
+
+;; public void Linq14()
+;; {
+;;    int[] numbersA = { 0, 2, 4, 5, 6, 8, 9 };
+;;    int[] numbersB = { 1, 3, 5, 7, 8 };
+;;
+;;    var pairs =
+;;        from a in numbersA
+;;        from b in numbersB
+;;        where a < b
+;;        select new { a, b };
+;;
+;;    Console.WriteLine("Pairs where a < b:");
+;;    foreach (var pair in pairs)
+;;    {
+;;        Console.WriteLine("{0} is less than {1}", pair.a, pair.b);
+;;    }
+;; }
+
+-----------------------------------------------)
+
+(defn linq14 []
+  (let [numbers-a [0 2 4 5 6 8 9]
+        numbers-b [1 3 5 7 8]
+        pairs (for [a numbers-a
+                    b numbers-b
+                    :when (< a b)]
+                {:a a, :b b})]
+    (println "Pairs where a < b: ")
+    (doseq [pair pairs]
+      (println (:a pair) " is less than " (:b pair)))))
+
+;;(linq14)
+
+(def examples [linq6 linq7a linq7b linq8 linq9a linq9b linq10 linq11 linq12 linq13 linq14])
