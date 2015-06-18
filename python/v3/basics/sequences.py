85cd41b0fe52eeceebdbd76834c3f48b5d9374e7
guptag <guptagaruda@gmail.com>
2015-06-17 18:24:49 -0700

    more examples in sequences

--- python/v3/basics/sequences.py
+++ python/v3/basics/sequences.py
@@ -128,3 +128,47 @@ print (sorted(fruits, key=len, reverse=True))
 print (fruits)
 fruits.sort()
 print (fruits)
+
+
+# Bisect module offers two functions bisect and insort that use binary search
+# algorithm to quickly find and insert items
+import bisect
+import sys
+HAYSTACK = [1, 4, 5, 6, 8, 12, 15, 20, 21, 23, 23, 26, 29, 30]
+NEEDLES = [0, 1, 2, 5, 8, 10, 22, 23, 29, 30, 31]
+ROW_FMT = '{0:2d} @ {1:2d}   {2}{0:<2d}'
+def demo (bisect_fn):
+    print('DEMO:', bisect_fn.__name__)
+    print('haystack->', ' '.join('%2d' % n for n in HAYSTACK))
+    for needle in reversed(NEEDLES):
+        position = bisect_fn(HAYSTACK, needle)
+        offset = position * '  |'
+        print(ROW_FMT.format(needle, position, offset))
+
+demo(bisect.bisect)
+demo(bisect.bisect_left)
+
+
+# Usage of Bisect - Given a test score, grade returns the
+# corresponding letter grade
+def grade(score, breakpoints=[60, 70, 80, 90], grades='FDCBA'):
+    i = bisect.bisect(breakpoints, score)
+    return grades[i]
+
+print([grade(score) for score in [33, 99, 77, 70, 89, 90, 100]])
+
+
+# insort(seq, item) insects an item into seq and
+# keeps the sequence in order
+
+import bisect
+import random
+
+SIZE = 7
+
+random.seed(1729)
+my_list = []
+for i in range(SIZE):
+    new_item = random.randrange(SIZE*2)
+    bisect.insort(my_list, new_item)
+    print('%2d ->' % new_item, my_list)
